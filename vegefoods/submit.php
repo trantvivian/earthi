3c410283d7f4f3e531a693965b733874ee1dbc41
Kevin Huynh <k.huynh1@yahoo.com>
2019-10-14 12:01:16 -0400

    working database

--- vegefoods/submit.php
+++ vegefoods/submit.php
@@ -1,7 +1,6 @@
 <?php $db_connection = pg_connect("host=ec2-184-73-232-93.compute-1.amazonaws.com dbname=dfr5diqgclghcj user=jiyfrexzyevjgb
 password=c62964fdc99205e14046ec51c65dc8effb2fde516b1c72aaa009deac12401aa5");
 
-
 $full_name = $_POST['full_name'];
 $email = $_POST['email'];
 $address = $_POST['address'];
@@ -11,7 +10,7 @@ $zipcode = $_POST['zipcode'];
 $password = $_POST['password'];
 $hashed_pasword = password_hash($password, PASSWORD_DEFAULT);
 
-$query = "INSERT into siteUsers VALUES ('$_POST[full_name]', '$email', '$address', '$city', '$state', '$zipcode', '$hashed_pasword')";
+$query = "INSERT into siteusers VALUES ('$full_name', '$email', '$address', '$city', '$state', '$zipcode', '$hashed_pasword')";
 
 $result = pg_query($db_connection, $query);
 
